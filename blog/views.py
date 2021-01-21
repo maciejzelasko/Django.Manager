51bf9526a519cf751520bff4457818357801805e
maciek.z <maciek.z@outlook.com>
2021-01-21 21:35:06 +0100

    Added template and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Paździoch',
+        'title': 'Kiepscy',
+        'content': 'First post content'
+    },
+    {
+        'author': 'Ferdek',
+        'title': 'Kiepscy',
+        'content': 'First post content'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Fake title'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', { 'title': 'About' })
